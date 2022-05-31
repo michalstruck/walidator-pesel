b4ae8550b7d023ca81cdeea79ebd8e9dbc093317
unknown <michal.o.struck@gmail.com>
2022-05-31 10:01:40 +0200

    feat: integrated hook-form, added Results styling and correct rendering

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,16 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import Results from "./Results";
+import { useForm } from "react-hook-form";
 
 interface Current {
   correct: boolean;
   pesel: string[];
   sex: boolean;
   DOB: string;
+}
+
+interface Form {
+  pesel: string;
 }
 const year = ["19", "19", "20", "20", "21", "21", "22", "22", "18", "18"];
 const monthSingleDigit = ["01", "02", "03", "04", "05", "06", "07", "08", "09"];
@@ -14,7 +19,12 @@ const weights = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3];
 
 const Main = () => {
   const [current, setCurrent] = useState({} as Current);
-
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Form>();
+  console.log(errors);
   const results = [
     {
       status: false,
@@ -33,16 +43,19 @@ const Main = () => {
   const checkPesel = (
     pesel: string
   ): { correct: boolean; peselArr: string[] } => {
+    let sum: number = 0;
+    const peselArr = pesel.split("");
+
     if (pesel === null || pesel.length !== 11)
       return { correct: false, peselArr: [] };
-    const peselArr = pesel.split("");
-    let sum: number = 0;
-    //todo -- rewrite to forEach
+
     for (let i: number = 0; i < peselArr.length - 1; i++) {
       sum += +peselArr[i] * weights[i];
     }
+
     const modulo = sum % 10;
     const twoLast = +pesel.substring(pesel.length - 1);
+
     return {
       correct: (modulo === 0 && twoLast === 0) || twoLast === 10 - modulo,
       peselArr,
@@ -63,48 +76,49 @@ const Main = () => {
     return +pesel[9] % 2 === 0;
   };
 
-  const handleSubmit = (e: React.BaseSyntheticEvent) => {
-    const { value } = e.target[0];
-    const { peselArr, correct } = checkPesel(value);
+  const Submit = (pesel: string) => {
+    const { peselArr, correct } = checkPesel(pesel);
     setCurrent(() => ({
       correct,
       pesel: peselArr,
       sex: getGender(peselArr),
       DOB: getDate(peselArr),
     }));
-    console.log(current);
   };
 
   return (
-    <div className="grid place-items-center h-screen">
+    <div className="flex items-center justify-center h-screen w-screen">
       <form
         className="bg-purple-400 w-fit h-fit grid place-items-center rounded-lg"
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit(e);
-        }}
+        onSubmit={handleSubmit((value) => Submit(value.pesel))}
       >
-        <label htmlFor="pesel" className="m-2">
+        <label className="m-2 text-neutral-800 drop-shadow-xl text-center">
           Pesel
+          <input
+            {...register("pesel", {
+              required: true,
+              pattern: /^[0-9]{11}/,
+              minLength: 11,
+              maxLength: 11,
+            })}
+            type="text"
+            className="text-center text-black block w-60 shadow-xl rounded-md required:"
+          />
         </label>
-        <input
-          id="pesel"
-          type="text"
-          className="block w-80 shadow-lg rounded-md focus:border-blue-100"
-          min="0"
-          required={true}
-        />
+
+        {/* todo: add popups for validation errors, fix layout */}
+
         <button
           type="submit"
           className="bg-fuchsia-700 rounded-md 
-         shadow-lg shadow-stone-500 h-fit w-fit p-2
+         shadow-lg shadow-neutral-800 h-fit w-fit p-2
          text-white container mx-36 my-8 active:translate-y-1"
         >
           Sprawdź
         </button>
         <button
           className="bg-fuchsia-700 rounded-md 
-         shadow-lg shadow-stone-500 h-fit w-fit p-2  
+         shadow-lg shadow-neutral-800 h-fit w-fit p-2  
          text-white container mx-36 my-8 active:translate-y-1"
           onClick={() => window.location.reload()}
         >
@@ -112,7 +126,7 @@ const Main = () => {
         </button>
         <span id="result" className="result"></span>
       </form>
-      <Results sex={current.sex} DOB={current.DOB} />
+      <Results sex={current.sex} DOB={current.DOB} correct={current.correct} />
     </div>
   );
 };
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,12 +1,16 @@
 type Props = {
   sex: boolean;
   DOB: string;
+  correct: boolean;
 };
 
-const Results = ({ sex, DOB }: Props) => {
+const Results = ({ sex, DOB, correct }: Props) => {
+  console.log(correct, sex);
   return (
-    <div>
-      {"Płeć: " + sex ? "Kobieta" : "🚹 Mężczyzna"} {DOB}
+    <div className="whitespace-pre-wrap bg-purple-400 text-neutral-800 w-fit h-fit grid place-items-center rounded-lg p-4">
+      Płeć: {correct && (sex ? "🚺 Kobieta" : "🚹 Mężczyzna")}
+      {"\n"}
+      Data urodzenia: {correct && DOB}
     </div>
   );
 };
